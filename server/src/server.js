be38b065fd92382486aac9584d92012a71a08de6
dihali <sannkendihali93@gmail.com>
2026-04-15 10:43:10 -0300

    Add global error handlers for uncaught exceptions
    Added error handling for uncaught exceptions and unhandled promise rejections.

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,11 @@
+process.on('uncaughtException', (err) => {
+    console.error('[ZapFlow] Uncaught exception:', err.message, err.stack);
+});
+
+process.on('unhandledRejection', (reason) => {
+    console.error('[ZapFlow] Unhandled rejection:', reason?.message || reason);
+});
+
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env'), override: true });
 const app = require('./app');
 
