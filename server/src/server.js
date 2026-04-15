b84f114a02c3ea1400812cd904a2a5f52d6a333b
dihali <sannkendihali93@gmail.com>
2026-04-15 11:00:55 -0300

    Change error logging to use stdout instead of stderr

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,22 @@
+process.stdout.write('[ZapFlow] Process starting...\n');
+
 process.on('uncaughtException', (err) => {
-    console.error('[ZapFlow] Uncaught exception:', err.message, err.stack);
+        process.stdout.write('[ZapFlow] Uncaught exception: ' + err.message + '\n' + err.stack + '\n');
 });
 
 process.on('unhandledRejection', (reason) => {
-    console.error('[ZapFlow] Unhandled rejection:', reason?.message || reason);
+        process.stdout.write('[ZapFlow] Unhandled rejection: ' + (reason?.message || String(reason)) + '\n');
 });
 
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env'), override: true });
+
+process.stdout.write('[ZapFlow] Loading app...\n');
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
+process.stdout.write('[ZapFlow] Binding to port ' + PORT + '...\n');
 
 app.listen(PORT, () => {
-  console.log(`[ZapFlow] Server running on port ${PORT}`);
-  console.log(`[ZapFlow] Env: ${process.env.NODE_ENV}`);
+        console.log('[ZapFlow] Server running on port ' + PORT);
+        console.log('[ZapFlow] Env: ' + process.env.NODE_ENV);
 });
